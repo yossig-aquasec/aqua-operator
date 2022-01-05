800f35ddc3fb6006d24962491f819768ef2c7b18
yossigilad-aquasec <yossi.gilad@aquasec.com>
2022-01-05 17:04:39 +0200

    adding starboard

---
+++ pkg/apis/aquasecurity/v1alpha1/clusterconfigauditreport_types.go
@@ -0,0 +1,84 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"github.com/aquasecurity/aqua-operator/pkg/apis/operator/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterConfigAuditReportSpec defines the desired state of ClusterConfigAuditReport
+type ClusterConfigAuditReportSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Infrastructure                *v1alpha1.AquaInfrastructure `json:"infra,omitempty"`
+	AllowAnyVersion               bool                         `json:"allowAnyVersion,omitempty"`
+	StarboardService              *v1alpha1.AquaService        `json:"deploy,required"`
+	Config                        v1alpha1.AquaStarboardConfig `json:"config"`
+	RegistryData                  *v1alpha1.AquaDockerRegistry `json:"registry,omitempty"`
+	ImageData                     *v1alpha1.AquaImage          `json:"image,omitempty"`
+	Envs                          []corev1.EnvVar              `json:"env,omitempty"`
+	LogDevMode                    bool                         `json:"logDevMode,omitempty"`
+	ConcurrentScanJobsLimit       string                       `json:"concurrentScanJobsLimit,omitempty"`
+	ScanJobRetryAfter             string                       `json:"scanJobRetryAfter,omitempty"`
+	MetricsBindAddress            string                       `json:"metricsBindAddress,omitempty"`
+	HealthProbeBindAddress        string                       `json:"healthProbeBindAddress,omitempty"`
+	CisKubernetesBenchmarkEnabled string                       `json:"cisKubernetesBenchmarkEnabled,omitempty"`
+	VulnerabilityScannerEnabled   string                       `json:"vulnerabilityScannerEnabled,omitempty"`
+	BatchDeleteLimit              string                       `json:"batchDeleteLimit,omitempty"`
+	BatchDeleteDelay              string                       `json:"batchDeleteDelay,omitempty"`
+}
+
+// ClusterConfigAuditReportStatus defines the observed state of ClusterConfigAuditReport
+type ClusterConfigAuditReportStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Nodes []string                     `json:"nodes"`
+	State v1alpha1.AquaDeploymentState `json:"state"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ClusterConfigAuditReport is the Schema for the clusterconfigauditreports API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=clusterconfigauditreports,scope=Namespaced
+type ClusterConfigAuditReport struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterConfigAuditReportSpec   `json:"spec,omitempty"`
+	Status ClusterConfigAuditReportStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ClusterConfigAuditReportList contains a list of ClusterConfigAuditReport
+type ClusterConfigAuditReportList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterConfigAuditReport `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterConfigAuditReport{}, &ClusterConfigAuditReportList{})
+}
---
+++ pkg/apis/aquasecurity/v1alpha1/configauditreport_types.go
@@ -0,0 +1,84 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"github.com/aquasecurity/aqua-operator/pkg/apis/operator/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ConfigAuditReportSpec defines the desired state of ConfigAuditReport
+type ConfigAuditReportSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Infrastructure                *v1alpha1.AquaInfrastructure `json:"infra,omitempty"`
+	AllowAnyVersion               bool                         `json:"allowAnyVersion,omitempty"`
+	StarboardService              *v1alpha1.AquaService        `json:"deploy,required"`
+	Config                        v1alpha1.AquaStarboardConfig `json:"config"`
+	RegistryData                  *v1alpha1.AquaDockerRegistry `json:"registry,omitempty"`
+	ImageData                     *v1alpha1.AquaImage          `json:"image,omitempty"`
+	Envs                          []corev1.EnvVar              `json:"env,omitempty"`
+	LogDevMode                    bool                         `json:"logDevMode,omitempty"`
+	ConcurrentScanJobsLimit       string                       `json:"concurrentScanJobsLimit,omitempty"`
+	ScanJobRetryAfter             string                       `json:"scanJobRetryAfter,omitempty"`
+	MetricsBindAddress            string                       `json:"metricsBindAddress,omitempty"`
+	HealthProbeBindAddress        string                       `json:"healthProbeBindAddress,omitempty"`
+	CisKubernetesBenchmarkEnabled string                       `json:"cisKubernetesBenchmarkEnabled,omitempty"`
+	VulnerabilityScannerEnabled   string                       `json:"vulnerabilityScannerEnabled,omitempty"`
+	BatchDeleteLimit              string                       `json:"batchDeleteLimit,omitempty"`
+	BatchDeleteDelay              string                       `json:"batchDeleteDelay,omitempty"`
+}
+
+// ConfigAuditReportStatus defines the observed state of ConfigAuditReport
+type ConfigAuditReportStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Nodes []string                     `json:"nodes"`
+	State v1alpha1.AquaDeploymentState `json:"state"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ConfigAuditReport is the Schema for the configauditreports API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=configauditreports,scope=Namespaced
+type ConfigAuditReport struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ConfigAuditReportSpec   `json:"spec,omitempty"`
+	Status ConfigAuditReportStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ConfigAuditReportList contains a list of ConfigAuditReport
+type ConfigAuditReportList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ConfigAuditReport `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ConfigAuditReport{}, &ConfigAuditReportList{})
+}
